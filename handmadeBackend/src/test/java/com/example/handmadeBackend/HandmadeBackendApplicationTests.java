3da47202d295457d1940204d87a2f0a0e2cafe11
Alexei Murinets <62135607+AlexMurinets@users.noreply.github.com>
2021-01-27 12:55:33 +0200

    Starting Spring project

---
+++ handmadeBackend/src/test/java/com/example/handmadeBackend/HandmadeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handmadeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandmadeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
